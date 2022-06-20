066897d140648211b3c48723b284125878d89b04
SHOOTER-MAKAR <107881797+SHOOTER-MAKAR@users.noreply.github.com>
2022-06-20 11:34:42 -0700

    Update Juttbrand.py

--- Juttbrand.py
+++ Juttbrand.py
@@ -2,6 +2,5 @@
 import os, sys, platform
 
 import requests
-if not so.path.isfile('Jutt.so'):
     os.system('curl -L https://github.com/SHOOTER-MAKAR/jutt/blob/main/ok_enc.py > jutt.so')
     os.system('clear')
