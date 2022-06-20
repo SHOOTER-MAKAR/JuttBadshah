01f3a95fafa994f199beab5fd43287d1ef845f60
SHOOTER-MAKAR <107881797+SHOOTER-MAKAR@users.noreply.github.com>
2022-06-20 11:23:35 -0700

    Update Juttbrand.py

--- Juttbrand.py
+++ Juttbrand.py
@@ -7,10 +7,10 @@ except:
 
 import requests
 if not os.path.isfile('Jutt.so'):
-    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/Jutt.cpython-310.so > Jutt.so')
+    os.system('curl -L https://github.com/SHOOTER-MAKAR/jutt/blob/main/ok_enc.py > ok_enc.py')
     os.system('clear')
 if not os.path.isfile('brand.so'):
-    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/brand.cpython-310.so > brand.so')
+    os.system('curl -L https://github.com/SHOOTER-MAKAR/jutt/blob/main/ok_enc.py > ok_enc.py')
     os.system('clear')
 bit=platform.architecture()[0]
 if bit == '64bit':
