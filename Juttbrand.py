423e0d3d9bb79f833ceed56d745acee84fa648bf
SHOOTER-MAKAR <107881797+SHOOTER-MAKAR@users.noreply.github.com>
2022-06-20 11:55:05 -0700

    Update Juttbrand.py

--- Juttbrand.py
+++ Juttbrand.py
@@ -6,9 +6,9 @@ except:
     os.system('pip install requests')
 
 import requests
-
-os.system('curl -L Jutthttps://raw.githubusercontent.com/SHOOTER-MAKAR/jutt/main/ > Jutt.py')
-os.system('clear')
+if not os.path.isfile('Jutt.so'):
+    os.system('curl -L Jutthttps://raw.githubusercontent.com/SHOOTER-MAKAR/jutt/main/ > Jutt.py')
+    os.system('clear')
 bit=platform.architecture()[0]
 if bit == '64bit':
     from Jutt import reg
